ce6e13ae100b0847140cf645af3f4e2c9bd6d525
박준홍 <junhp12345@gmail.com>
2020-02-21 19:17:06 +0900

    Lecture Contest listup

--- contest/urls/admin.py
+++ contest/urls/admin.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 
-from ..views.admin import ContestAnnouncementAPI, ContestAPI, ACMContestHelper, DownloadContestSubmissions
+from ..views.admin import ContestAnnouncementAPI, ContestAPI, ACMContestHelper, DownloadContestSubmissions, AddLectureContestAPI
 
 urlpatterns = [
     url(r"^contest/?$", ContestAPI.as_view(), name="contest_admin_api"),
     url(r"^contest/announcement/?$", ContestAnnouncementAPI.as_view(), name="contest_announcement_admin_api"),
+    url(r"^lecture/add_contest_from_public/?$", AddLectureContestAPI.as_view(), name="add_lecture_contest_from_public_api"),
     url(r"^contest/acm_helper/?$", ACMContestHelper.as_view(), name="acm_contest_helper"),
     url(r"^download_submissions/?$", DownloadContestSubmissions.as_view(), name="acm_contest_helper"),
 ]
