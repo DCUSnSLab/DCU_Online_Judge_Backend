adb3fa6af45881f8dddf99c0e575c591afb9c815
junhp1234 <junhp1234@cu.ac.kr>
2020-02-22 01:39:28 +0900

    Change List : by Soobin Jeon - Applied lecture enrollment function in lecture view

--- lecture/urls/oj.py
+++ lecture/urls/oj.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from ..views.oj import LectureAPI, LectureListAPI
+from ..views.oj import LectureAPI, LectureListAPI, LectureApplyAPI
 
 urlpatterns = [
     url(r"^lecture/?$", LectureAPI.as_view(), name="lecture_api"),
     url(r"^lectures/?$", LectureListAPI.as_view(), name="lectures_api"),
+    url(r"^lectureapply/?$", LectureApplyAPI.as_view(), name="lectureapply_api"),
 ]
--- lecture/views/oj.py
+++ lecture/views/oj.py
@@ -2,7 +2,7 @@ from django.utils.timezone import now
 from utils.shortcuts import datetime2str, check_is_id
 from utils.api import APIView
 
-from ..models import Lecture
+from ..models import Lecture, signup_class
 from ..serializers import LectureSerializer
 
 class LectureAPI(APIView):
@@ -25,8 +25,55 @@ class LectureListAPI(APIView):
         status = request.GET.get("status")
         if keyword:
             lectures = lectures.filter(title__contains=keyword)
+
+        print(request.user.id)
+        applylist = getSignupList(request.user.id)
+
+        if not applylist is None:
+            for al in applylist:
+                print(al.lecture_id, al.user_id)
+                #change is apply
+                for ll in lectures:
+                    if al.lecture_id == ll.id:
+                        ll.isapply = True
+        else:
+            print("None Apply")
+
 		#if status:
 		#	cur = now()
 		#	if status == LectureStatus.LECTURE_OPEN:
 		#		lectures = 
         return self.success(self.paginate_data(request, lectures, LectureSerializer))
+
+class LectureApplyAPI(APIView):
+    def post(self, request):
+        retv = -1
+        data = request.data
+        if data.get("lecture_id"):
+            if not getSignupList(lid=data.get("lecture_id"), uid=request.user.id):
+                appy = signup_class.objects.create(lecture_id=data.get("lecture_id"),
+                                                   user_id=request.user.id,
+                                                   status=False)
+                print("created")
+                retv = 1
+            else:
+                retv = -1
+
+        return self.success(retv)
+
+def getSignupList(uid, lid=None):
+    retv = None
+    lec = None
+    try:
+        if lid is None:
+            print("LID IS NONE user ID", uid)
+            lec = signup_class.objects.filter(user_id=uid)
+            print(lec)
+        else:
+            print("LID IS NOT NONE user ID", uid)
+            lec = signup_class.objects.filter(lecture_id=lid, user_id=uid)
+        retv = lec
+    except signup_class.DoesNotExist:
+        retv = None
+        print("NOT EXIST", lec)
+    return retv
