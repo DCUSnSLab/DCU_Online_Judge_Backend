efd2438b16c96e10c2a87636d4998b04d4c9500a
박준홍 <junhp12345@gmail.com>
2020-03-04 15:07:08 +0900

    Change List: 관리자 페이지 기능 구현 및 메인 페이지 출력 오류 수정

--- lecture/urls/oj.py
+++ lecture/urls/oj.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 
-from ..views.oj import LectureAPI, LectureListAPI, LectureApplyAPI
+from ..views.oj import LectureAPI, LectureListAPI, TakingLectureListAPI, LectureApplyAPI
 
 urlpatterns = [
     url(r"^lecture/?$", LectureAPI.as_view(), name="lecture_api"),
     url(r"^lectures/?$", LectureListAPI.as_view(), name="lectures_api"),
+    url(r"^takinglec/?$", TakingLectureListAPI.as_view(), name="takinglectures_api"),
     url(r"^lectureapply/?$", LectureApplyAPI.as_view(), name="lectureapply_api"),
 ]
