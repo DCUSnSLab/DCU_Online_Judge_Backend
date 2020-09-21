7672e96ce325861ef354ddd4b03eb183f994d3dd
jin <kairos0@cu.ac.kr>
2020-09-22 00:24:37 +0900

    ChangeList: QnA 기능 업데이트

--- qna/urls/oj.py
+++ qna/urls/oj.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from ..views.oj import QnAPostAPI, QnAPostDetailAPI
+from ..views.oj import QnAPostAPI, QnAPostDetailAPI, CommentAPI
 
 urlpatterns = [
+    url(r"^comment/?$", CommentAPI.as_view(), name="Comment_API"),
     url(r"^qapost/?$", QnAPostAPI.as_view(), name="QnAPost_API"),
     url(r"^qapostdetail/?$", QnAPostDetailAPI.as_view(), name="QnAPostDetail_API"),
 ]
