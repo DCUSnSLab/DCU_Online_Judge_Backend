f5f0d74d86d72e52b441f7c5802f1dadba141b56
Hijae Song <hijae99@gmail.com>
2023-05-17 10:41:35 +0900

    fix mkdir

--- submission/views/oj.py
+++ submission/views/oj.py
@@ -118,7 +118,7 @@ class SubmissionAPI(APIView):
             print("git clone")
             print(request.user.username)
             git_dir = os.path.join(settings.GIT_PATH, request.user.username)
-            os.mkdir(git_dir,exist_ok=True)
+            os.mkdir(git_dir)
             # os.makedirs('temp', exist_ok=True)
             repo = Repo.clone_from("https://github.com/"+ str(request.user.username) +"/EduCoder.git", git_dir)
             repo.git.checkout("master")
