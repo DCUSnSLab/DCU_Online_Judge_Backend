338b39c978f0a8d770da45a7aac2f293248b7027
backgroundmin <zxc81808080@gmail.com>
2024-04-24 07:03:40 +0000

    except add

--- utils/DBTasks/changeSampleColumValue.py
+++ utils/DBTasks/changeSampleColumValue.py
@@ -10,21 +10,24 @@ def changeValue():
     try:
         print("try get data")
         Problems = Problem.objects.all()
-        print("probles Get")
+        print("problems Get")
         for problem in Problems:
-            test_case_dir = os.path.join(settings.TEST_CASE_DIR, problem.test_case_id)
-            newSampleData = []
-            sampleLen = len(problem.samples)
-            for i in range(sampleLen):
-                testCaseData = {}
-                with open(test_case_dir + '/' + str(i+1) + '.in', "r") as f:
-                    testCaseData['input'] = ''.join(f.readlines()).strip()
-                with open(test_case_dir + '/' + str(i+1) + '.out', "r") as f:
-                    testCaseData['output'] = ''.join(f.readlines()).strip()
-                newSampleData.append(testCaseData)
-            problem.samples = newSampleData
-            problem.save()
-            print("problem.id: ", problem.id , "success save")
-    except:
+            try:
+                test_case_dir = os.path.join(settings.TEST_CASE_DIR, problem.test_case_id)
+                newSampleData = []                                                        
+                sampleLen = len(problem.samples)
+                for i in range(sampleLen):      
+                    testCaseData = {}     
+                    with open(test_case_dir + '/' + str(i+1) + '.in', "r") as f:
+                        testCaseData['input'] = ''.join(f.readlines()).strip()  
+                    with open(test_case_dir + '/' + str(i+1) + '.out', "r") as f:
+                        testCaseData['output'] = ''.join(f.readlines()).strip()  
+                    newSampleData.append(testCaseData)                         
+                problem.samples = newSampleData       
+                problem.save()                 
+                print("problem.id: ", problem.id , "success save")
+            except:
+                print("problem.id: ", problem, "save error")
+    except:                                                   
         print("except")
-changeValue()
+changeValue()          
