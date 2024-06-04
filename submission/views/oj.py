69c2370f009e395a1bf85e5b83b7d230d19aac68
backgroundmin <zxc81808080@gmail.com>
2024-06-04 07:44:28 +0000

    Reverted to previous commit

--- submission/views/oj.py
+++ submission/views/oj.py
@@ -110,7 +110,7 @@ class SubmissionAPI(APIView):
         # run result return
         try:
             if data["sample_test"]:
-                submissionResultData = JudgeDispatcher(submission.id, problem.id, data["sample_count"]).judge()
+                submissionResultData = JudgeDispatcher(submission.id, problem.id).judge()
                 outputResultData = []
                 if isinstance(submissionResultData, list):
                     for i in range(data["sample_count"]):
