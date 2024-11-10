f543b0c283f9fccd57ff5e1eb78faa62e18d066e
Hanbaek <totwjfakd@gmail.com>
2024-11-10 17:30:34 +0900

    ch return:

--- submission/views/admin.py
+++ submission/views/admin.py
@@ -45,4 +45,4 @@ class SubmissionDateAPI(APIView):
         # 결과를 [{"date": "YYYY-MM-DD", "submission_count": int}, ...] 형식으로 변환
         data = [{"date": submission['date'].strftime('%Y-%m-%d'), "submission_count": submission['submission_count']} for submission in submission_counts]
         
-        return self.success(data=data)
+        return self.success(data)
